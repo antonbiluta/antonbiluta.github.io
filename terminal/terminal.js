133a675a2cfd8a62bbb5af35a0f4349baa9d8842
antonbiluta <AntonBiluta@gmail.com>
2022-03-05 16:15:31 +0300

    Update css, html, js

--- terminal/terminal.js
+++ terminal/terminal.js
@@ -19,3 +19,77 @@ async function onload() {
         main();
     }
 }
+
+async function debug() {
+	const { power } = await import("./util/power.js");
+	const { main } = await import("./util/screens.js");
+	const { parse } = await import("./util/io.js");
+	power();
+	main();
+	parse("fallout");
+}
+
+function togglePower() {
+	let isOff = document.getElementById("crt").classList.contains("off");
+	if (isOff) {
+		on();
+	} else {
+		off();
+	}
+}
+
+function handleClick(event) {
+	if (event) {
+		event.preventDefault();
+	}
+	let input = document.querySelector("[contenteditable='true']");
+	if (input) {
+		input.focus();
+	}
+}
+
+function fly(event) {
+	event.target.classList.toggle("fly");
+}
+
+function theme(event) {
+	click();
+	let theme = event.target.dataset.theme;
+	[...document.getElementsByClassName("theme")].forEach(b =>
+		b.classList.toggle("active", false)
+	);
+	event.target.classList.add("active");
+	document.body.classList = "theme-" + theme;
+	handleClick();
+}
+
+function fullscreen(event) {
+	toggleFullscreen();
+	event.target.blur();
+}
+
+function globalListener({ keyCode }) {
+	if (keyCode === 122) {
+		// F11
+		toggleFullscreen();
+	} else if (keyCode === 27) {
+		// ESC
+		toggleFullscreen(false);
+	}
+}
+document.addEventListener("keydown", globalListener);
+document.getElementById("dial").addEventListener("input", event => {
+	let value = event.target.value;
+	setVolume(value);
+});
+
+// Define some stuff on the window so we can use it directly from the HTML
+Object.assign(window, {
+	debug,
+	onload,
+	togglePower,
+	theme,
+	fly,
+	handleClick,
+	fullscreen
+});
