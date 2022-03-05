7d8c68c578a1518ca79a334fb2ca503fda674a9d
antonbiluta <AntonBiluta@gmail.com>
2022-03-05 15:44:59 +0300

    add terminal.js

---
+++ terminal/terminal.js
@@ -0,0 +1,5 @@
+import { setVolume } from "./util/speak.js";
+import { click } from "./sound/index.js";
+import { on, off } from "./util/power.js";
+import { toggleFullscreen } from "./util/screens.js";
+import { type } from "./util/io.js";
